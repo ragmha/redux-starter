0742a7da045f176f113faac7348af9080c83e118
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-02 05:20:47 +0300

    init

---
+++ readme.md
@@ -0,0 +1,15 @@
+# 🦄 redux starter
+> Quickly get Redux up and running
+
+
+## Getting Started 🚀
+
+* ```$ git clone https://github.com/ragmha/redux-starter.git```
+
+* ```$ yarn```
+
+* ```$ yarn start -s```
+
+
+## License
+[MIT](./license) © [Raghib Hasan](http://raghibm.com/)
---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1> 🦄 </h1>
+          <h1>redux-starter</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
