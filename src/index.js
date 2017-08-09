d2bb08e93762f8548c4e3f8cf7459395cfc32ce8
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-09 11:22:38 +0300

    Fix typo and add redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import App from './components/app';
@@ -12,7 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
   <Provider store={store}>
